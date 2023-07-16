32841550f9eba14a11bb2169e068bb02c03a3ef5
Deshitha199 <IT20657628@my.sliit.lk>
2023-07-16 14:18:58 +0530

    Initial setup commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import React, {useState} from 'react';
-import List from "./List";
-import data from "./data";
+import React from 'react';
 
 function App() {
 
-  const [people, setPeople] = useState(data);
-
   return (
     <div className="container">
-      <h3>{people.length} Birth Days Today</h3>
-        <List people={people} />
-      <button onClick={() => setPeople([])}>Clear All</button>
     </div>
   );
 }
